363119107bdae36c690813a2242be76a33bbde87
Ayaan087 <148577426+Ayaan087@users.noreply.github.com>
2023-10-28 16:13:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+let ans = await inquirer.prompt([
+    {
+        name: "Word",
+        type: "input",
+        message: "Enter the Input"
+    }
+]);
+let words = ans.sentence.Trim().split(" ");
+console.log(`Your sentence word count is ${words.length}`);
